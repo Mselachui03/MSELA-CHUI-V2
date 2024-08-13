e6e0fcad9a0c5b8c615cf7f96b466aea2fa382b2
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-08-14 02:57:23 +0300

    Update stickersearch.js

--- commandes/stickersearch.js
+++ commandes/stickersearch.js
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'BONIPHACE-MD',
+      author: 'MSELA-CHUI-V2',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
