101494b9c43aed4a51bf3c5c9fdacec30004e587
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-08-14 02:43:19 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -7,7 +7,7 @@ const { default: axios } = require('axios');
 zokou({ nomCom: 'ping',
     desc: 'To check ping',
     Categorie: 'General',
-    reaction: '🚀', 
+    reaction: '💥', 
     fromMe: 'true', 
 
        
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*BONIPHACE-MD 🚀 respond speed is*\n ```' +6890+ '``` *m/s*') 
+    return repondre('*MSELA-CHUI-V2 🚀 respond speed is*\n ```' +6890+ '``` *m/s*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
