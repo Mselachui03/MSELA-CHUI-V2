63cf3ecd1b79a62d4df221a2f730bdf0eba83755
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-08-14 02:01:57 +0300

    Update fancy.js

--- commandes/fancy.js
+++ commandes/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "☑️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 boniphace tech\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('BONIPHACE-MD', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 MSELA-CHUI-V2\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('MSELA-CHUI-V2', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
